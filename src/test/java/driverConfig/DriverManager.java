1cdc552ab2b9fb7d88e1d2418ceee40fb7581873
Diego Bustamante <inc.diego.bustamante@gmail.com>
2023-12-01 09:53:39 -0300

    linux cappabilities ChromeOptions

--- src/test/java/driverConfig/DriverManager.java
+++ src/test/java/driverConfig/DriverManager.java
@@ -11,12 +11,21 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class DriverManager {
     private static WebDriver driver;
 
-    protected void createDriver(Navegador nav){
+    protected void createDriver(Navegador nav) {
         System.out.println("Inicializando navegador: " + nav.toString());
-        switch (nav){
+        String os = System.getProperty("os.name").toLowerCase();
+        switch (nav) {
             case Chrome:
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
+                if (os.contains("linux")) {
+                    chromeOptions.addArguments("--ignore-certificate-errors");
+                    chromeOptions.addArguments("--disable-extensions");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--headless");
+                }
                 chromeOptions.addArguments("--remote-allow-origins=*");
                 driver = new ChromeDriver(chromeOptions);
                 break;
@@ -31,7 +40,8 @@ public class DriverManager {
         }
         driver.manage().window().maximize();
     }
-    protected WebDriver getDriver(){
+
+    protected WebDriver getDriver() {
         return driver;
     }
 }
